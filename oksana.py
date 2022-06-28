25e11044d44e9eb4ed9ba58d9b5b199ffdf96247
Ivan Bartenev <ivan-bartenyov@yandex.ru>
2022-06-28 19:27:54 +0300

    Задача на скорость

---
+++ oksana.py
@@ -0,0 +1,15 @@
+from typing import List
+
+def odometer(data: List[int])-> int:
+    "Return distanse from start"
+    distance = 0
+
+    for i in range(0, len(data), 2):
+        if i == 0:
+            distance += data[i]*data[i+1]
+        else:
+            distance += data[i]*(data[i+1]-data[i-1])
+
+    return distance
+
+print(odometer(data = [10, 1, 20, 2]))
