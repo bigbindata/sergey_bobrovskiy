4f8c2e5a7052ab2a354ec8714f5ddb2359e47fbb
Ivan Bartenev <ivan-bartenyov@yandex.ru>
2022-07-01 16:45:37 +0300

    явно указал сохранение списка

--- Mad_Max.py
+++ Mad_Max.py
@@ -1,7 +1,8 @@
 from typing import List
 
 def MadMax(N: int, Tele: List[int]) -> List[int]:
-    if len(Tele)==1:
+    if len(Tele) == 1:
         return Tele
-    sorted(Tele)
+    Tele=sorted(Tele, reverse = False)
+    
     return Tele[:N//2]+Tele[:N//2-1:-1]
