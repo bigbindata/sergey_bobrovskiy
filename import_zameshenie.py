df60429bbb3a2a7d6e979c3faf7ae303a4547833
Ivan Bartenev <ivan-bartenyov@yandex.ru>
2022-07-23 15:08:18 +0300

    Сделал более питонячим и поправил redo

--- import_zameshenie.py
+++ import_zameshenie.py
@@ -1,89 +1,81 @@
-from collections import deque
-
-class ConditionProgrammDeque():
+class ConditionProgrammList():
 
     def __init__ (self, list_string_condition, position, last_command):
-        self._deque_string_condition = deque(list_string_condition)
+        self._list_string_condition = list_string_condition
         self._position = position
         self._last_command = last_command
         self._count_use_undo = 0
         self._count_use_append_or_del = 0
 
-
-    def get_count_use_undo(self,):
+    @property
+    def count_use_undo(self,):
         return self._count_use_undo
 
-    def set_count_use_undo(self, value):
+    @count_use_undo.setter
+    def count_use_undo(self, value):
         self._count_use_undo = value
+        if self._count_use_undo < 0:
+            self._count_use_undo = 0
 
-    def increment_count_use_undo(self,):
-        self._count_use_undo += 1
-    
-    def decrement_count_use_undo(self,):
-        self._count_use_undo -= 1
-
-    def get_count_use_append_or_del(self,):
+    @property
+    def count_use_append_or_del(self,):
         return self._count_use_append_or_del
 
-    def set_count_use_append_or_del(self, value):
+    @count_use_append_or_del.setter
+    def count_use_append_or_del(self, value):
         self._count_use_append_or_del = value
+        if self._count_use_append_or_del < 0:
+            self._count_use_append_or_del = 0
 
-    def increment_count_use_append_or_del(self,):
-        self._count_use_append_or_del += 1
-
-    def decrement_count_use_append_or_del(self,):
-        self._count_use_append_or_del -= 1
-
-    def get_position(self,):
+    @property
+    def position(self,):
         return self._position
     
-    def set_position(self, value):
+    @position.setter
+    def position(self, value):
         self._position = value
- 
-    def increment_position(self,):
-        self._position += 1
-        self._deque_string_condition.rotate(1)
-        
+        if self._position < 0:
+            self._position = 0
+        if self._position > len(self._list_string_condition) - 1:
+            self._position = len(self._list_string_condition) - 1
 
-    def decrement_position(self,):
-        self._position -= 1
-        self._deque_string_condition.rotate(-1)
+    @property
+    def list_string_condition(self,):
+        return self._list_string_condition
 
-
-    def set_list_string_condition(self, value):
-        self._deque_string_condition = deque(value)
-
-    def get_list_string_condition(self,):
-        return self._deque_string_condition
-    
+    @list_string_condition.setter
+    def list_string_condition(self, value):
+        self._list_string_condition = list(value)
+  
     def append_to_string_condition(self, value):
-        self._deque_string_condition.append(value)
+        self._list_string_condition.append(value)
+
+    @property
+    def current_string_condition(self,):
+        return self._list_string_condition[self._position]
     
-    def get_current_string_condition(self,):
-        return self._deque_string_condition[0]
-    
-    def set_last_command(self, value):
-        self._last_command = value
-    
-    def get_last_command(self):
+    @property
+    def last_command(self):
         return self._last_command
 
+    @last_command.setter
+    def last_command(self, value):
+        self._last_command = value
 
 def append_string(S: str) -> str:
     global global_string
-    if condition_programm.get_count_use_undo()>0:
-        condition_programm.set_position(0)
-        condition_programm.set_list_string_condition([global_string, ])
-        condition_programm.set_count_use_undo(0)
-        condition_programm.set_count_use_append_or_del(0)
+    if condition_programm.count_use_undo > 0:
+        condition_programm.position = 0
+        condition_programm.list_string_condition = [global_string, ]
+        condition_programm.count_use_undo = 0
+        condition_programm.count_use_append_or_del = 0
 
-    condition_programm.append_to_string_condition(condition_programm.get_current_string_condition() + S)
+    condition_programm.append_to_string_condition(condition_programm.current_string_condition + S)
+    condition_programm.position += 1
+    condition_programm.last_command = 1
+    condition_programm.count_use_append_or_del += 1
 
-    condition_programm.increment_position()
-    condition_programm.set_last_command(1)
-    condition_programm.increment_count_use_append_or_del()
-
-    global_string = condition_programm.get_current_string_condition()
+    global_string = condition_programm.current_string_condition
     return global_string
 
 
@@ -91,30 +83,29 @@ def del_symbols(N:str) -> str:
     global global_string
     N = int(N)
 
-    if condition_programm.get_count_use_undo()>0:
-        condition_programm.set_position(0)
-        condition_programm.set_list_string_condition([global_string, ])
-        condition_programm.set_count_use_undo(0)
-        condition_programm.set_count_use_append_or_del(0)
+    if condition_programm.count_use_undo > 0:
+        condition_programm.position = 0
+        condition_programm.list_string_condition = [global_string, ]
+        condition_programm.count_use_undo = 0
+        condition_programm.count_use_append_or_del = 0
 
     if N >= len(global_string):
         condition_programm.append_to_string_condition("")
     else:
         condition_programm.append_to_string_condition(global_string[:-N])
 
-    condition_programm.increment_position()
-    condition_programm.set_last_command(2)
-    condition_programm.increment_count_use_append_or_del()
+    condition_programm.position += 1
+    condition_programm.last_command = 2
+    condition_programm.count_use_append_or_del += 1
 
-
-    global_string = condition_programm.get_current_string_condition()
+    global_string = condition_programm.current_string_condition
     return global_string
 
 
 def return_symbol_by_index(i:str) -> str:
     global global_string
     i = int(i)
-    condition_programm.set_last_command(3)
+    condition_programm.last_command = 3
     if i > len(global_string)-1:
         return ""
     return global_string[i]
@@ -123,12 +114,12 @@ def return_symbol_by_index(i:str) -> str:
 def Undo():
     global global_string
 
-    if condition_programm.get_last_command() in (1, 2, 4, 5) and condition_programm.get_count_use_append_or_del() > 0:
-        condition_programm.decrement_position()
-        condition_programm.decrement_count_use_append_or_del()
-        global_string = condition_programm.get_current_string_condition()
-    condition_programm.set_last_command(4)
-    condition_programm.increment_count_use_undo()
+    if condition_programm.last_command in (1, 2, 4, 5) and condition_programm.count_use_append_or_del > 0:
+        condition_programm.position -= 1
+        condition_programm.count_use_append_or_del -= 1
+        global_string = condition_programm.current_string_condition
+    condition_programm.last_command = 4
+    condition_programm.count_use_undo += 1
     
     return global_string
 
@@ -136,13 +127,13 @@ def Undo():
 def Redo():
     global global_string
 
-    if condition_programm.get_last_command() in (4, 5) and condition_programm.get_count_use_undo() > 0:
-        condition_programm.increment_position()
-        condition_programm.decrement_count_use_undo()
-        global_string = condition_programm.get_current_string_condition()
-        condition_programm.increment_count_use_append_or_del()
+    if condition_programm.last_command in (4, 5) and condition_programm.count_use_undo > 0:
+        condition_programm.position += 1
+        condition_programm.count_use_undo -= 1
+        global_string = condition_programm.current_string_condition
+        condition_programm.count_use_append_or_del += 1
 
-    condition_programm.set_last_command(5)
+    condition_programm.last_command = 5
     return global_string
 
 
@@ -168,6 +159,7 @@ def BastShoe(command: str) -> str:
         dict_function[number_command](arguments)
         return global_string
 
-
 global_string = ""
-condition_programm = ConditionProgrammDeque(["",],0, None)
+condition_programm = ConditionProgrammList(["",],0, None)
+
+
